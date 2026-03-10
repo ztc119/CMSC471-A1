d3b4778e06670c3186751555f77828cc2e0d6d41
ztc119 <tsaizx89@gmail.com>
2026-03-10 00:40:37 -0400

    Update script.js

--- erica/script.js
+++ erica/script.js
@@ -4,8 +4,9 @@ const height = 500 - margin.top - margin.bottom;
 
 const svg = d3.select('#vis')
     .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
+    .attr('viewBox', `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+    .attr('width', '100%')          // Makes it scale horizontally
+    .attr('height', 'auto')         // Optional, ensures proportional scaling
     .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
 
@@ -122,7 +123,7 @@ d3.csv("summary_crime.csv").then(data => {
 });
 
 function renderLegend(svg, width, color, periods) {
-    const legend = svg.append("g").attr("transform", `translate(${width + 20}, 0)`);
+    const legend = svg.append("g").attr("transform", `translate(${width * 0.9}, 0)`);
     periods.forEach((p, i) => {
         const g = legend.append("g").attr("transform", `translate(0,${i * 25})`);
         g.append("rect").attr("width", 18).attr("height", 18).attr("fill", color(p));
